ea9b680fceeb12dadceec57d09f7c61c47d81ec7
mabhikumar <mabhikumar66@gmail.com>
2023-02-15 22:00:23 +0530

    first commit

--- project32/settings.py
+++ project32/settings.py
@@ -126,3 +126,10 @@ MEDIA_URL='media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='valih825'
+EMAIL_HOST_PASSWORD='gnbpcsipaquulxwp'
+EMAIL_USE_TLS=True
+EMAIL_USE_SSL=False
+EMAIL_PORT=587
